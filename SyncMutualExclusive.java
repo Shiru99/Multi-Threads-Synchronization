200a5d5419d7c50bc4fcb18278ebbf72c3343a2d
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-09 19:35:03 +0530

    Mutual Exclusive Synchronization

--- SyncMutualExclusive.java
+++ SyncMutualExclusive.java
@@ -7,7 +7,8 @@
 
 */
 
-class ThreadDetails {
+class Stats {
+
     synchronized static void details() {
         for (int i = 0; i < 5; i++) {
             System.out.println(Thread.currentThread().getId() + " : " + Thread.currentThread().getName());
@@ -26,7 +27,7 @@ class ThreadDetails {
         System.out.println();
 
         // critical section starts
-        synchronized(this)      // This -> same object Table-td
+        synchronized(this)                  // This -> same object Table-td
         {
             for (int i = 0; i < 5; i++)
                 System.out.println("☠️ Critical Section of "+ Thread.currentThread().getName());
@@ -43,7 +44,7 @@ class ThreadDetails {
             System.out.print("*");
         System.out.println();
 
-        synchronized(ThreadDetails.class)  // critical section in static function
+        synchronized(Stats.class)  // critical section in static function
         {
             for (int i = 0; i < 5; i++)
                 System.out.println("☠️ Critical Section of "+ Thread.currentThread().getName());
@@ -55,51 +56,39 @@ class ThreadDetails {
     }
 }
 
-class Thread1 extends Thread {
-    ThreadDetails td ;
-    public Thread1(String string, ThreadDetails td) {
+class Thread0 extends Thread {
+
+    Stats td ;
+    
+    public Thread0(String string, Stats td) {
         super(string);
         this.td =td; 
     }
 
     @Override
     public void run() {
-        System.out.println("Thread 1 Started....");
-        ThreadDetails.details();
-        td.moreDetails();
-        td.trashWithDetails();
-        System.out.println("Thread 1 Finished....");
-    }
-}
 
-class Thread2 extends Thread {
-    ThreadDetails td ;
-    public Thread2 (String string, ThreadDetails td) {
-        super(string);
-        this.td=td;
-    }
+        System.out.println(Thread.currentThread().getName()+" Started....");
 
-    @Override
-    public void run() {
-        System.out.println("Thread 2 Started....");
-        ThreadDetails.details();    
-        td.moreDetails();           
-        td.trashWithDetails();   
-        System.out.println("Thread 2 Finished....");
+        Stats.details();    // sync static method
+        td.moreDetails();           // sync instance method
+        td.trashWithDetails();      // sync block in method
+
+        System.out.println(Thread.currentThread().getName()+" Finished....");
+
         /*
             sync is bet. same critical section of all threads
             but not in different critical sections of same or different threads
-        */   
+        */  
     }
 }
 
-public class MutualExclusiveSync {
+public class SyncMutualExclusive {
     public static void main(String[] args) 
     {
-        
-        ThreadDetails td = new ThreadDetails();
-        Thread thread1 = new Thread1("Thread 1",td);
-        Thread thread2 = new Thread2("Thread 2",td);
+        Stats td = new Stats();
+        Thread thread1 = new Thread0("Thread 1",td);
+        Thread thread2 = new Thread0("Thread 2",td);
         thread1.start();
         thread2.start();
     }
