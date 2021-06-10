6c4c107ac9abb9d8ea67b84be70f760dafdeebc1
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-10 14:14:26 +0530

    conditional sync using synchronized, wait & notify

--- SyncConditional.java
+++ SyncConditional.java
@@ -1,11 +1,12 @@
 // Producer-Consumer Problem
 
-class ProductionPlant{
+class ProductionPlant {
+
     int product;
     boolean isProductReady = false; // Semaphore - binary
 
-    synchronized void produce(int product){
-        if(isProductReady)
+    synchronized void produce(int product) {
+        if (isProductReady)
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -13,28 +14,28 @@ class ProductionPlant{
             }
 
         this.product = product;
-        System.out.println("Product Produced - "+this.product);
+        System.out.println("Product Produced - " + this.product);
         isProductReady = true;
         notify();
     }
 
-    synchronized void consume(){
-        if(!isProductReady)
+    synchronized void consume() {
+        if (!isProductReady)
             try {
                 wait();
-            }catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        System.out.println("Product Consumed - "+this.product);
+        System.out.println("Product Consumed - " + this.product);
         isProductReady = false;
         notify();
     }
 
 }
 
-class Producer extends Thread
-{
+class Producer extends Thread {
     ProductionPlant productionPlant;
+
     public Producer(String string, ProductionPlant productionPlant) {
         super(string);
         this.productionPlant = productionPlant;
@@ -44,18 +45,19 @@ class Producer extends Thread
     public void run() {
         super.run();
         for (int i = 1; i < 6; i++) {
-            try {
-                sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            productionPlant.produce(i*i);
+            // try {
+            // sleep(100);
+            // } catch (InterruptedException e) {
+            // e.printStackTrace();
+            // }
+            productionPlant.produce(i * i);
         }
     }
 }
-class Consumer extends Thread
-{
+
+class Consumer extends Thread {
     ProductionPlant productionPlant;
+
     public Consumer(String string, ProductionPlant productionPlant) {
         super(string);
         this.productionPlant = productionPlant;
@@ -65,11 +67,11 @@ class Consumer extends Thread
     public void run() {
         super.run();
         for (int i = 1; i < 6; i++) {
-            try {
-                sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            // try {
+            // sleep(100);
+            // } catch (InterruptedException e) {
+            // e.printStackTrace();
+            // }
             productionPlant.consume();
         }
     }
@@ -79,10 +81,10 @@ public class SyncConditional {
     public static void main(String[] args) {
         ProductionPlant productionPlant = new ProductionPlant();
 
-        Thread p = new Producer("Producer",productionPlant);
-        Thread c = new Consumer("Consumer",productionPlant);
+        Thread p = new Producer("Producer", productionPlant);
+        Thread c = new Consumer("Consumer", productionPlant);
 
         p.start();
-        c.start(); 
+        c.start();
     }
 }
